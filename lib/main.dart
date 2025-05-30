339aaa92326203dc7d04dc94cf402c3f0d281fb2
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2025-05-30 15:32:16 +0900

    Change Card theme

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
             title: 'Namer App',
             theme: ThemeData(
               useMaterial3: true,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
             ),
             home: MyHomePage()));
   }
@@ -59,7 +59,10 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Text(pair.asLowerCase),
