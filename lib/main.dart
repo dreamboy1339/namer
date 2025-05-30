12323cdeb699f2733e6d9cc5d9b95603db43e568
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2025-05-30 16:17:28 +0900

    Apply center align

--- lib/main.dart
+++ lib/main.dart
@@ -34,18 +34,22 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              print("button pressed!");
-              appState.getNext();
-            },
-            child: Text("Next !!!"),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Text('A random AWESOME idea:'),
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                print("button pressed!");
+                appState.getNext();
+              },
+              child: Text("Next !!!"),
+            ),
+          ],
+        ),
       ),
     );
   }
